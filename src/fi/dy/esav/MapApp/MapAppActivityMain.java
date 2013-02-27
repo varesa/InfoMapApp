908be8b0b21803fece19eb93f596d4d660dd9385
Esa Varemo <esa@kuivanto.fi>
2013-02-27 16:12:49 +0200

    List available locationproviders to console

--- src/fi/dy/esav/MapApp/MapAppActivityMain.java
+++ src/fi/dy/esav/MapApp/MapAppActivityMain.java
@@ -1,6 +1,10 @@
 package fi.dy.esav.MapApp;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MapAppActivityMain extends Activity {
@@ -11,5 +15,21 @@ public class MapAppActivityMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        getStatus();
+        
+    }
+    
+    int getStatus() {
+    	LocationManager manager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        List<String> providers = manager.getAllProviders();
+        for(String provider : providers) {
+        	System.out.println("Available location provider: " + provider);
+        	Location loc = manager.getLastKnownLocation(provider);
+        	//loc.getAccuracy()
+        	
+        }
+        
+        return 0;
     }
 }
